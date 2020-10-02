d9fae3bd0d0b56569441e1f63b91f73e83d2c4ce
wys-wys <63457826+wys-wys@users.noreply.github.com>
2020-10-02 23:27:00 +0800

    添加项目文件。

---
+++ dameo/dameo.cpp
@@ -0,0 +1,15 @@
+/*
+Hook （钩子）
+（1）进程钩子
+（2）全局钩子
+windows 消息机制
+*/
+#include <Windows.h> //包含了许多windows api
+//cui 控制台应用程序 =》 main
+//gui 图形界面=》winmain
+
+//HINSTANCE
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPreInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+	return 0;
+}
